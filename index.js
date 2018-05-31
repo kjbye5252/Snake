7c2d31a0e979ca50f7a3b925c0b5b23d36e8f464
kjbye5252 <37642604+kjbye5252@users.noreply.github.com>
2018-05-31 08:56:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,15 @@
-var weight = 0.5;
-var slider;
-var slider2;
-var slider3;
+var grid = [[0,1,2,3,4,5,6,7,8,9],[0,1,2,3,4,5,6,7,8,9]]
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  slider = createSlider(1,230,5,1);
-  slider.position((width/2)-65,0);
-  slider2 = createSlider(0.01,0.75,0.5,0.01);
-  slider2.position((width/2)-65,25);
-  slider3 = createSlider(1,250,200,1);
-  slider3.position((width/2)-65,50);
-  angleMode(DEGREES);
+  createCanvas(400,400);
 }
 
 function draw() {
   background(51);
-  translate(width/2,height/2);
-  stroke(255);
-  strokeWeight(weight);
-  cross(slider3.value(),slider.value());
-  for (i = 0; i < 3; i++) {
-    translate(slider3.value(),0);
-    rotate(90);
-    cross(slider3.value(),slider.value());
-  }
 }
 
-function cross(length,iterations) {
-  line(0,0,-length,0);
-  translate(-length,0);
-  if (length > iterations) {
-    push();
-    rotate(90);
-    cross(length * slider2.value(),iterations);
-    pop();
-    push();
-    rotate(0);
-    cross(length * slider2.value(),iterations);
-    pop();
-    push();
-    rotate(-90);
-    cross(length * slider2.value(),iterations);
-    pop();
+function grid() {
+  for(i=0;i>10;i++){
+    line(0,i*40,400,i*40);
   }
 }
