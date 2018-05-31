8b662e02f96ca85faea5dab96296b7ba2df03aee
kjbye5252 <37642604+kjbye5252@users.noreply.github.com>
2018-05-31 09:03:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var grid = [[0,1,2,3,4,5,6,7,8,9],[0,1,2,3,4,5,6,7,8,9]]
+var grid = [[0,1,2,3,4,5,6,7,8,9],[0,1,2,3,4,5,6,7,8,9]];
 
 function setup() {
   createCanvas(400,400);
@@ -6,6 +6,7 @@ function setup() {
 
 function draw() {
   background(51);
+  line(0,0,25,25);
   grid();
 }
 
