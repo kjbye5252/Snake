11e42ec8ff2f82201f01d43f665b63cae43b995d
kjbye5252 <37642604+kjbye5252@users.noreply.github.com>
2018-05-31 09:20:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-//var grid = [[0,1,2,3,4,5,6,7,8,9],[0,1,2,3,4,5,6,7,8,9]];
+var location = [[0],[0]];
 
 function setup() {
   createCanvas(400,400);
-  stroke(255);
 }
 
 function draw() {
   background(51);
   grid();
+  snake();
+}
+
+function snake() {
+  stroke(255);
+  rect(location[0][0],location[1][0],location[0][0]+40,location[1][0]+40);
 }
 
 function grid() {
+  stroke(0,255,0);
   for(var i=0;i<10;i++) {
     line(0,i*40,400,i*40);
+  }
+  for(var i=0;i<10;i++) {
+    line(i*40,0,i*40,400);
   }
 }
