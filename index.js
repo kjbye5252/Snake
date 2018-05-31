a450c46ba756c1000b0bd21c00edbdf0dd447ec9
kjbye5252 <37642604+kjbye5252@users.noreply.github.com>
2018-05-31 09:05:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var grid = [[0,1,2,3,4,5,6,7,8,9],[0,1,2,3,4,5,6,7,8,9]];
 
 function setup() {
   createCanvas(400,400);
+  stroke(255);
 }
 
 function draw() {
@@ -12,8 +13,6 @@ function draw() {
 
 function grid() {
   for(i=0;i<10;i++){
-    stroke(255);
-    strokeWeight(5);
-    line(0,-40,400,-40);
+    line(0,40,400,40);
   }
 }
