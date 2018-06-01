0a48144e1daa27bb78ec07e35211262b801f7d67
kjbye5252 <37642604+kjbye5252@users.noreply.github.com>
2018-06-01 09:44:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var snakeX = [1,0];
 var snakeY = [0,0];
-var foodX = random(1,5);
-var foodY = random(1,5);
+var foodX = math.round(random(1,5));
+var foodY = math.round(random(1,5));
 var direction = 1;
 
 function setup() {
@@ -74,8 +74,8 @@ function tail(){
 
 function food(){
   if(snakeX == foodX && snakeY == foodY){
-    foodX = random(0,19);
-    foodY = random(0,19);
+    foodX = math.round(random(0,19));
+    foodY = math.round(random(0,19));
   }
   stroke(255,0,0);
   rect(foodX*30,foodY*30,29,29);
